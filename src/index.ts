cf365905315d67759bbd7b6ebd8fc19d3b8f630a
CallyRoad <cally.dev@outlook.fr>
2024-11-18 14:20:44 +0100

    feat : update configuration

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import express, {Express, Request, Response} from "express";
-import {port} from "./env.js";
-
-// import * as process from "process";
+import {port} from "./env";
 
 const app: Express = express();
 
 app.use(express.json());
 
-app.get("/ok", (req: Request , res: Response) => {
+app.get("/ok", (req: Request, res: Response) => {
     res.send("Server ok")
 });
 
 app.listen(port, () => {
     console.log(`[server] : Server is running at ${port}`);
 });
+
